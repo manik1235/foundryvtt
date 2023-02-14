0f73e6e2f5989f9482184a1658b62994f68f511c
Andrew Henning-Kolberg <andrew.kolberg@gmail.com>
2023-02-14 23:26:22 +0000

    Convert to docker-compose
    This commit converts the script to a docker compose file

--- foundry-docker.sh
+++
@@ -1,5 +0,0 @@
-docker run \
-  --env FOUNDRY_RELEASE_URL='https://foundryvtt.s3.amazonaws.com/releases/10.291/FoundryVTT-10.291.zip?AWSAccessKeyId=AKIA2KJE5YZ3BCQQ6DH3&Signature=wjZoSdGB0jVw%2BfIlrorOC%2BHaFr4%3D&Expires=1676347400' \
-  --publish 30000:30000/tcp \
-  --volume /docker/foundryvtt/data:/data \
-  felddy/foundryvtt:release
